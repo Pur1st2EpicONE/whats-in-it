2cf7c520e263428316c0cb27bae4d7a180033bc0
Nikita <cherrylj@student.21-school.ru>
2025-07-25 21:11:02 +0300

    add runtime check for LLM provider

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,10 +1,13 @@
 package client
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/Pur1st2EpicONE/whats-in-it/internal/client/gigachat"
+	"github.com/Pur1st2EpicONE/whats-in-it/internal/client/yandex"
 	"github.com/Pur1st2EpicONE/whats-in-it/internal/models"
+	"github.com/spf13/viper"
 )
 
 type GPT interface {
@@ -18,5 +21,14 @@ type ChatClient struct {
 }
 
 func NewChatClient() *ChatClient {
-	return &ChatClient{GPT: gigachat.NewGigaChat()}
+	currentModel := viper.GetString("current_model")
+	switch currentModel {
+	case "giga_chat":
+		return &ChatClient{GPT: gigachat.NewGigaChat()}
+	case "yandex_gpt":
+		return &ChatClient{GPT: yandex.NewYandexGPT()}
+	default:
+		log.Fatalf("unknown GPT model: %s", currentModel)
+		return nil
+	}
 }
